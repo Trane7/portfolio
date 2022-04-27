6d52bec18255360cef56715630b458d9bae758f8
Dee <dgrowette@gmail.com>
2022-04-27 13:50:16 -0700

    changed virtual notes to a potion porject and added a potion image

--- portfolio/src/components/portfolio/Portfolio.jsx
+++ portfolio/src/components/portfolio/Portfolio.jsx
@@ -8,31 +8,39 @@ export default function Portfolio() {
         <li className="active">Projects</li>
       </ul>
       <div className="container">
-        <div className="item">
+        <div className="item"> {/* Plenty App*/}
           <img src={process.env.PUBLIC_URL + "/assets/plenty.png"} alt="" /> {/* this is where you add the project*/}
           <h3><a href="https://plenty-in-the-pantry.herokuapp.com/">Plenty.</a></h3>
           <h4><a href="https://github.com/Trane7/Recipe-app">Github Repo</a></h4>
-          <p> This is a recipe app! Tech: HTML5, CSS3, JavaScript, React Native, Node.js, Express.js.</p>
+          <p> This is a recipe app that uses: HTML5, CSS3, JavaScript, React Native, Node.js, and Express.js.</p>
         </div>
-        <div className="item">
+        <div className="item"> {/* Console this App*/}
           <img src={process.env.PUBLIC_URL + "/assets/consolethis.png"} alt="" /> {/* this is where you add the project*/}
           <h3><a href="https://trane7.github.io/Console-This/">Console This</a></h3>
+          <h4><a href="https://github.com/Trane7/Console-This">Github Repo</a></h4>
+          <p></p>
         </div>
-        <div className="item">
+        <div className="item">  {/* Quick Quack Quiz App*/}
           <img src={process.env.PUBLIC_URL + "/assets/quick-quack.png"} alt="" /> {/* this is where you add the project     https://quick-quack-quiz.herokuapp.com/*/} 
           <h3><a href="https://quick-quack-quiz.herokuapp.com/">Quick Quack Quiz</a></h3>
+          <h4><a href="https://github.com/Trane7/Recipe-app">Github Repo</a></h4>
+          <p></p>
         </div>
-        <div className="item">
+        <div className="item"> {/* Budget Tracker App*/}
           <img src={process.env.PUBLIC_URL + "/assets/budget.png"} alt="" /> {/* this is where you add the project*/}
           <h3><a href="https://budget-tracker-pwas.herokuapp.com/">Budget Tracker</a></h3>
+          <h4><a href="https://github.com/Trane7/Recipe-app">Github Repo</a></h4>
+          <p></p>
         </div>
-        <div className="item">
+        <div className="item">  {/* Weather App*/}
           <img src={process.env.PUBLIC_URL + "/assets/weather.png"} alt="" /> {/* this is where you add the project*/}
           <h3><a href="https://trane7.github.io/Weather-or-not/">Weather App</a></h3>
+          <h4><a href="https://github.com/Trane7/Recipe-app">Github Repo</a></h4>
         </div>
-        <div className="item">
-          <img src={process.env.PUBLIC_URL + "/assets/virtual.png"} alt="" /> {/* this is where you add the project*/}
-          <h3><a href="https://virtualnote.herokuapp.com/">Virtual Notes</a></h3>
+        <div className="item">  {/* Don't Forget The Potion Game*/}
+          <img src={process.env.PUBLIC_URL + "/assets/potion.png"} alt="" /> {/* this is where you add the project*/}
+          <h3><a href="https://trane7.github.io/dont-forget-the-potion/">Don't Forget The Potion!</a></h3>
+          <h4><a href="https://github.com/Trane7/Recipe-app">Github Repo</a></h4>
         </div>
         <div className="item">
           <img src={process.env.PUBLIC_URL + "/assets/schedule.png"} alt="" /> {/* this is where you add the project*/}
