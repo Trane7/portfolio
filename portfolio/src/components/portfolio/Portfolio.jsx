a1735c6b7e5e53204c5aff4a80f0d315b5ca37cb
Dee <dgrowette@gmail.com>
2022-04-27 15:12:41 -0700

    fixed the portfolio links, added github repo, and replaced some projects with others

--- portfolio/src/components/portfolio/Portfolio.jsx
+++ portfolio/src/components/portfolio/Portfolio.jsx
@@ -12,40 +12,51 @@ export default function Portfolio() {
           <img src={process.env.PUBLIC_URL + "/assets/plenty.png"} alt="" /> {/* this is where you add the project*/}
           <h3><a href="https://plenty-in-the-pantry.herokuapp.com/">Plenty.</a></h3>
           <h4><a href="https://github.com/Trane7/Recipe-app">Github Repo</a></h4>
-          <p> This is a recipe app that uses: HTML5, CSS3, JavaScript, React Native, Node.js, and Express.js.</p>
+          <p>This is a Full-Stack recipe app that helps the user find recipes based off the ingredients they currently have in the house.</p>
+          <p>Technologies: HTML5, CSS3, JavaScript, React Native, Node.js, Heroku, and Express.js.</p>
         </div>
         <div className="item"> {/* Console this App*/}
           <img src={process.env.PUBLIC_URL + "/assets/consolethis.png"} alt="" /> {/* this is where you add the project*/}
           <h3><a href="https://trane7.github.io/Console-This/">Console This</a></h3>
           <h4><a href="https://github.com/Trane7/Console-This">Github Repo</a></h4>
-          <p> Using JavaScript, HTML, CSS, and MVP, this app was designed to help the user find a gaming console close to home!</p>
+          <p>This app was designed to help the user find any kind of gaming console close to home.</p>
+          <p>Technologies: JavaScript, HTML, CSS, MVP, Node.js, Express.js, and APIs.</p>
         </div>
         <div className="item">  {/* Quick Quack Quiz App*/}
           <img src={process.env.PUBLIC_URL + "/assets/quick-quack.png"} alt="" /> {/* this is where you add the project     https://quick-quack-quiz.herokuapp.com/*/} 
           <h3><a href="https://quick-quack-quiz.herokuapp.com/">Quick Quack Quiz</a></h3>
           <h4><a href="https://github.com/Trane7/Quick-Quack-Quiz">Github Repo</a></h4>
           <p>This is a Full-Stack web developed app that was made to help people make quizzes to help them study and to share with others.</p>
+          <p>Technologies: JavaScript, HTML, CSS, Handlebars.js, Express.js, Node.js, Heroku, and MVP.</p>
         </div>
         <div className="item">  {/* Don't Forget The Potion Game*/}
           <img src={process.env.PUBLIC_URL + "/assets/potion.png"} alt="" /> {/* this is where you add the project*/}
           <h3><a href="https://trane7.github.io/dont-forget-the-potion/">Don't Forget The Potion!</a></h3>
           <h4><a href="https://github.com/Trane7/dont-forget-the-potion">Github Repo</a></h4>
+          <p>Don't Forget the Potion is a fun little game that the user plays through the terminal.</p>
+          <p>Technologies: JavaScript, JSON, Jest, and JavaScript.</p>
         </div>
         <div className="item"> {/* Robot Gladiators */}
           <img src={process.env.PUBLIC_URL + "/assets/robot.png"} alt="" /> 
           <h3><a href="https://trane7.github.io/robot-gladiators/">Robot Gladiators</a></h3>
           <h4><a href="https://github.com/Trane7/robot-gladiators">Github Repo</a></h4>
+          <p>Robot Gladiators is a prompt based game.
+          </p>
+          <p>Technologies: JavaScript, HTML, CSS, MVP.</p>
         </div>
         <div className="item"> {/* Budget Tracker App*/}
           <img src={process.env.PUBLIC_URL + "/assets/budget.png"} alt="" /> {/* this is where you add the project*/}
           <h3><a href="https://budget-tracker-pwas.herokuapp.com/">Budget Tracker</a></h3>
           <h4><a href="https://github.com/Trane7/Recipe-app">Github Repo</a></h4>
-          <p></p>
+          <p>Budget Tracker is an app that helps the user budget their expenses on a daily basis.</p>
+          <p>Technologies: JavaScript, HTML, CSS, PWA.</p>
         </div>
         <div className="item">  {/* Weather App*/}
           <img src={process.env.PUBLIC_URL + "/assets/weather.png"} alt="" /> {/* this is where you add the project*/}
           <h3><a href="https://trane7.github.io/Weather-or-not/">Weather App</a></h3>
           <h4><a href="https://github.com/Trane7/Recipe-app">Github Repo</a></h4>
+          <p>The Weather App is, well of couse a weather app to tell the user what the current 5-day forcast will be.</p>
+          <p>Technologies: JavaScript, HTML, CSS.</p>
         </div>
         
       </div>
