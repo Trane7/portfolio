a0329746d24de248784e25ca7174af170e632d95
Dee <dgrowette@gmail.com>
2022-04-27 14:44:47 -0700

    added to consoleThis and to quick quack quiz in portfolio files

--- portfolio/src/components/portfolio/Portfolio.jsx
+++ portfolio/src/components/portfolio/Portfolio.jsx
@@ -18,18 +18,23 @@ export default function Portfolio() {
           <img src={process.env.PUBLIC_URL + "/assets/consolethis.png"} alt="" /> {/* this is where you add the project*/}
           <h3><a href="https://trane7.github.io/Console-This/">Console This</a></h3>
           <h4><a href="https://github.com/Trane7/Console-This">Github Repo</a></h4>
-          <p></p>
+          <p> Using JavaScript, HTML, CSS, and MVP, this app was designed to help the user find a gaming console close to home!</p>
         </div>
         <div className="item">  {/* Quick Quack Quiz App*/}
           <img src={process.env.PUBLIC_URL + "/assets/quick-quack.png"} alt="" /> {/* this is where you add the project     https://quick-quack-quiz.herokuapp.com/*/} 
           <h3><a href="https://quick-quack-quiz.herokuapp.com/">Quick Quack Quiz</a></h3>
-          <h4><a href="https://github.com/Trane7/Recipe-app">Github Repo</a></h4>
-          <p></p>
+          <h4><a href="https://github.com/Trane7/Quick-Quack-Quiz">Github Repo</a></h4>
+          <p>This is a Full-Stack web developed app that was made to help people make quizzes to help them study and to share with others.</p>
         </div>
         <div className="item">  {/* Don't Forget The Potion Game*/}
           <img src={process.env.PUBLIC_URL + "/assets/potion.png"} alt="" /> {/* this is where you add the project*/}
           <h3><a href="https://trane7.github.io/dont-forget-the-potion/">Don't Forget The Potion!</a></h3>
-          <h4><a href="https://github.com/Trane7/Recipe-app">Github Repo</a></h4>
+          <h4><a href="https://github.com/Trane7/dont-forget-the-potion">Github Repo</a></h4>
+        </div>
+        <div className="item"> {/* Robot Gladiators */}
+          <img src={process.env.PUBLIC_URL + "/assets/robot.png"} alt="" /> 
+          <h3><a href="https://trane7.github.io/robot-gladiators/">Robot Gladiators</a></h3>
+          <h4><a href="https://github.com/Trane7/robot-gladiators">Github Repo</a></h4>
         </div>
         <div className="item"> {/* Budget Tracker App*/}
           <img src={process.env.PUBLIC_URL + "/assets/budget.png"} alt="" /> {/* this is where you add the project*/}
@@ -42,11 +47,7 @@ export default function Portfolio() {
           <h3><a href="https://trane7.github.io/Weather-or-not/">Weather App</a></h3>
           <h4><a href="https://github.com/Trane7/Recipe-app">Github Repo</a></h4>
         </div>
-        <div className="item">
-          <img src={process.env.PUBLIC_URL + "/assets/schedule.png"} alt="" /> 
-          <h3><a href="https://trane7.github.io/Day-to-Remember/">Daily Planner</a></h3>
-          <h4><a href="https://github.com/Trane7/Day-to-Remember">Github Repo</a></h4>
-        </div>
+        
       </div>
     </div>
   )
