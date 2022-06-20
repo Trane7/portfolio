7f57d94eea00db236420f2bc5f1faff508704674
Dee <dgrowette@gmail.com>
2022-06-20 16:22:43 -0700

    fixed the about me section

--- portfolio/src/components/works/Works.jsx
+++ portfolio/src/components/works/Works.jsx
@@ -7,20 +7,20 @@ export default function Works() {
       <div className="card">
         <p>
           Welcome! Thank you for taking the time out of your day to view my
-          progress so far. My name is Demetre Growette, I am a young Minnesota
+          progress so far. My name is Demetre Growette, I am a young Minnesotan
           adult living the California life. I decided that I needed a change of
           direction and finally pursing something that I believe is right for
-          me. In order to better myself for myself, I chose to challenge myself
-          by entering the world of coding and push myself to become what
-          everyone else told me I wouldn't.
-          I've recently finished my stint at U.C. Davis and persuing a career as
+          me. In order to better myself, I chose to challenge myself
+          by entering the world of coding and push to become what
+          everyone else told me I couldn't.
+          I've recently finished my bootcamp at U.C. Davis and persuing a career as
           a full stack web developer. Throughout my time with U.C Davis, I've
           gained experience on gaining technical programming skills in HTML5,
           CSS3, JavaScript, JQuery, Bootstrap, Node Js, MySQL,
           MongoDB, Express, Handelbars.js, Python, C#, Angular, AWS, Node, GraphQL, Java, & ReactJS. Prior to to my bootcamp
           experience I had only had beginner knowledge of Python, but with my new
-          found love for web development. I look forward to learning and
-          creating more for the world.
+          found love for web development, I look forward to learning and
+          creating to broaden my knowledge in coding!
         </p>
       </div>
     </div>
