925a8a77ba438259914dc4ffa5bfbad72caca4d8
Dee <dgrowette@gmail.com>
2022-08-18 15:23:13 -0700

    changed the theme of the webpage entirely

--- portfolio/src/components/works/Works.jsx
+++ portfolio/src/components/works/Works.jsx
@@ -7,20 +7,20 @@ export default function Works() {
       <div className="card">
         <p>
           Welcome! Thank you for taking the time out of your day to view my
-          progress so far. My name is Demetre Growette, I am a young Minnesotan
-          adult living the California life. I decided that I needed a change of
-          direction and finally pursing something that I believe is right for
-          me. In order to better myself, I chose to challenge myself
-          by entering the world of coding and push to become what
-          everyone else told me I couldn't.
-          I've recently finished my bootcamp at U.C. Davis and persuing a career as
-          a full stack web developer. Throughout my time with U.C Davis, I've
+          progress so far. My name is Demetre Growette, I am a very outgoing
+          individual who is easy to work with and tends to make work life more
+          exciting. Last year I decided that I needed a change of direction and
+          am finally pursing something that I believe is right path for me. In
+          order to better myself, I chose to enter the world of coding and push
+          to become what everyone else told me I couldn't. A little over a year
+          ago I finished my bootcamp at U.C. Davis and persuing a career as a
+          Front-End Web Developer. Throughout my time with U.C. Davis, I've
           gained experience on gaining technical programming skills in HTML5,
-          CSS3, JavaScript, JQuery, Bootstrap, Node Js, MySQL,
-          MongoDB, Express, Handelbars.js, Python, C#, Angular, AWS, Node, GraphQL, Java, & ReactJS. Prior to to my bootcamp
-          experience I had only had beginner knowledge of Python, but with my new
-          found love for web development, I look forward to learning and
-          creating to broaden my knowledge in coding!
+          CSS3, JavaScript, JQuery, Bootstrap, Node.js, AWS, GraphQL, &
+          React.js. I am currently working as a part-time Teaching Assistant at
+          2U Trinity Learning Center Coding Bootcamps and found a new love for
+          web development and I look forward to learning and creating more
+          intuitive projects to expan my knowledge in coding!
         </p>
       </div>
     </div>
