3c5823b71c5c7e9faa53912570df5f20eb5ae82d
Dee <dgrowette@gmail.com>
2022-08-18 13:31:28 -0700

    added itype feature and started to change the colors of portfolio

--- portfolio/src/components/intro/Intro.jsx
+++ portfolio/src/components/intro/Intro.jsx
@@ -1,6 +1,20 @@
 import "./intro.scss";
+import { init } from 'ityped';
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+
+  const textRef = useRef();
+
+  useEffect (() =>{           //This is for the itype feature 
+    init(textRef.current, {
+      showCursor: false,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Developer", "Designer", "Content Creator"],
+    })
+  },[])
+
   return (
     <div className= "intro" id="intro">
         <div className="left">
@@ -12,7 +26,7 @@ export default function Intro() {
           <div className="wrapper">
             <h2>Hello, I'm</h2>
             <h1>Demetre Growette</h1>
-            <h3>Full-Stack Web Developer<span></span></h3>
+            <h3>Front-End Web <span ref={textRef}></span></h3>
           </div>
           <a href="#portfolio">
             <img src="assets/down.png" alt="" />
