34c10cfccaa3712a806203e8a59160689bdc1e10
Dee <dgrowette@gmail.com>
2022-08-24 12:46:02 -0700

    switched my picture adn text

--- portfolio/src/components/intro/Intro.jsx
+++ portfolio/src/components/intro/Intro.jsx
@@ -18,12 +18,7 @@ export default function Intro() {
   return (
     <div className= "intro" id="intro">
         <div className="left">
-          <div className="imgContainer">
-            <img src={process.env.PUBLIC_URL + "/assets/me.png"} alt=""/>
-          </div>
-        </div>
-        <div className="right">
-          <div className="wrapper">
+        <div className="wrapper">
             <h2>Hello, I'm</h2>
             <h1>Demetre Growette</h1>
             <h3>Front-End Web <span ref={textRef}></span></h3>
@@ -31,6 +26,12 @@ export default function Intro() {
           <a href="#portfolio">
             <img src="assets/down.png" alt="" />
           </a>
+        </div>
+        <div className="right">
+        <div className="imgContainer">
+            <img src={process.env.PUBLIC_URL + "/assets/me.png"} alt=""/>
+          </div>
+         
         </div>
     </div>
   )
