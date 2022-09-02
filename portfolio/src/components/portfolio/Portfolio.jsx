9dba49b2b19511f915b090d712da5a9a907683dc
Dee <dgrowette@gmail.com>
2022-09-02 11:47:37 -0700

    finished the modals for projects/portfolio section

--- portfolio/src/components/portfolio/Portfolio.jsx
+++ portfolio/src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,7 @@
 // import { Button } from "bootstrap";
 import "./portfolio.scss";
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 
 // const style = {
@@ -18,9 +16,22 @@ import Modal from '@mui/material/Modal';
 
 export default function Portfolio() {
 
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  const [modal1, setOpen] = React.useState(false);
+  const handle1 = () => setOpen(true);
+  const close1 = () => setOpen(false);
+
+  const [modal2, setOpen2] = React.useState(false);
+  const handle2 = () => setOpen2(true);
+  const close2 = () => setOpen2(false);
+
+  const [modal3, setOpen3] = React.useState(false);
+  const handle3 = () => setOpen3(true);
+  const close3 = () => setOpen3(false);
+  
+  const [modal4, setOpen4] = React.useState(false);
+  const handle4 = () => setOpen4(true);
+  const close4 = () => setOpen4(false); 
+
 
   return (
     <div className= "portfolio" id="portfolio">
@@ -30,10 +41,10 @@ export default function Portfolio() {
       </ul>
       <div className="container">
       <div className="item"> {/* CTV Radio Modal*/}   
-          <Button onClick={handleOpen}><img src={process.env.PUBLIC_URL + '/assets/ctvlogosm.jpg'} className='ctv-btn'></img></Button>
-          <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
+          <Button onClick={handle1}><img src={process.env.PUBLIC_URL + '/assets/ctvlogosm.jpg'} className='ctv-btn'></img></Button>
+          <Modal open={modal1} onClose={close1} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
               <div className="popout">
-                <button className="xb" onClick={handleClose}> X </button>
+                <button className="xb" onClick={close1}> X </button>
                 <br></br>
                 <img src={process.env.PUBLIC_URL + "/assets/ctvlogosm.jpg"} alt="" /> {/* this is where you add the project*/}
                 <br></br>
@@ -47,10 +58,10 @@ export default function Portfolio() {
             </Modal>
         </div>
       <div className="item"> {/* Plenty Modal*/}   
-          <Button onClick={handleOpen} data-overlay='pro1'><img src={process.env.PUBLIC_URL + '/assets/plenty.png'} className='plenty-btn'></img></Button>
-          <Modal open={open} onClose={handleClose} id='plenty' aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
+          <Button onClick={handle2} data-overlay='pro1'><img src={process.env.PUBLIC_URL + '/assets/plenty.png'} className='plenty-btn'></img></Button>
+          <Modal open={modal2} onClose={close2} id='plenty' aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
               <div className="popout">
-                <button className="xb" onClick={handleClose}> X </button>
+                <button className="xb" onClick={close2}> X </button>
                 <br></br>
                 <img src={process.env.PUBLIC_URL + "/assets/plenty.png"} alt="" /> {/* this is where you add the project*/}
                 <br></br>
@@ -63,44 +74,52 @@ export default function Portfolio() {
               </div>
             </Modal>
         </div>
-        <div className="item"> {/* Console this App*/}
-          <img src={process.env.PUBLIC_URL + "/assets/consolethis.png"} alt="" /> {/* this is where you add the project*/}
-          <h3><a href="https://trane7.github.io/Console-This/">Console This</a></h3>
-          <h4><a href="https://github.com/Trane7/Console-This">Github Repo</a></h4>
-          <p>This app was designed to help the user find any kind of gaming console close to home.</p>
-          <p>Technologies: JavaScript, HTML, CSS, MVP, Node.js, Express.js, and APIs.</p>
+      <div className="item"> {/* Console This Modal*/}   
+          <Button onClick={handle3} data-overlay='pro1'><img src={process.env.PUBLIC_URL + '/assets/consolethis.png'} className='console-btn'></img></Button>
+          <Modal open={modal3} onClose={close3} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
+              <div className="popout">
+                <button className="xb" onClick={close3}> X </button>
+                <br></br>
+                <img src={process.env.PUBLIC_URL + "/assets/consolethis.png"} alt="" /> {/* this is where you add the project*/}
+                <br></br>
+                <div>
+                <button className="btn1"><a href="https://trane7.github.io/Console-This/" className="link1">Console This</a></button>
+                <button className="btn2"><a href="https://github.com/Trane7/Console-This" className="link2">Github Repo</a></button>
+                </div>
+                <p>This app was designed to help the user find any kind of gaming console close to home.</p>
+                <p>Technologies: JavaScript, HTML, CSS, MVP, Node.js, Express.js, and APIs.</p>
+              </div>
+            </Modal>
         </div>
-        <div className="item">  {/* Quick Quack Quiz App*/}
-          <img src={process.env.PUBLIC_URL + "/assets/quick-quack.png"} alt="" /> {/* this is where you add the project     https://quick-quack-quiz.herokuapp.com/*/} 
-          <h3><a href="https://quick-quack-quiz.herokuapp.com/">Quick Quack Quiz</a></h3>
-          <h4><a href="https://github.com/Trane7/Quick-Quack-Quiz">Github Repo</a></h4>
-          <p>This is a Full-Stack web developed app that was made to help people make quizzes to help them study and to share with others.</p>
-          <p>Technologies: JavaScript, HTML, CSS, Handlebars.js, Express.js, Node.js, Heroku, and MVP.</p>
+      <div className="item"> {/* Quick Quack Quiz Modal*/}   
+          <Button onClick={handle4} data-overlay='pro1'><img src={process.env.PUBLIC_URL + '/assets/quick-quack2.png'} className='qq-btn'></img></Button>
+          <Modal open={modal4} onClose={close4} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
+              <div className="popout">
+                <button className="xb" onClick={close4}> X </button>
+                <br></br>
+                <img src={process.env.PUBLIC_URL + "/assets/quick-quack2.png"} alt="" /> {/* this is where you add the project*/}
+                <br></br>
+                <div>
+                <button className="btn1"><a href="https://quick-quack-quiz.herokuapp.com/" className="link1">Quick-Quack</a></button>
+                <button className="btn2"><a href="https://github.com/Trane7/Quick-Quack-Quiz" className="link2">Github Repo</a></button>
+                </div>
+                <p>This is a Full-Stack web developed app that was made to help people make quizzes to help them study and to share with others.</p>
+                <p>Technologies: JavaScript, HTML, CSS, Handlebars.js, Express.js, Node.js, Heroku, and MVP.</p>
+              </div>
+            </Modal>
         </div>
-        <div className="item">  {/* Don't Forget The Potion Game*/}
-          <img src={process.env.PUBLIC_URL + "/assets/potion.png"} alt="" /> {/* this is where you add the project*/}
-          <h3><a href="https://trane7.github.io/dont-forget-the-potion/">Don't Forget The Potion!</a></h3>
-          <h4><a href="https://github.com/Trane7/dont-forget-the-potion">Github Repo</a></h4>
-          <p>Don't Forget the Potion is a fun little game that the user plays through the terminal.</p>
-          <p>Technologies: JavaScript, JSON, Jest, and JavaScript.</p>
-        </div>
-        <div className="item"> {/* Robot Gladiators */}
-          <img src={process.env.PUBLIC_URL + "/assets/robot.png"} alt="" /> 
-          <h3><a href="https://trane7.github.io/robot-gladiators/">Robot Gladiators</a></h3>
-          <h4><a href="https://github.com/Trane7/robot-gladiators">Github Repo</a></h4>
-          <p>Robot Gladiators is a prompt based game.
-          </p>
-          <p>Technologies: JavaScript, HTML, CSS, MVP.</p>
-        </div>
-        <div className="item">  {/* Weather App*/}
-          <img src={process.env.PUBLIC_URL + "/assets/weather.png"} alt="" /> {/* this is where you add the project*/}
-          <h3><a href="https://trane7.github.io/Weather-or-not/">Weather App</a></h3>
-          <h4><a href="https://github.com/Trane7/Weather-or-not">Github Repo</a></h4>
-          <p>The Weather App is, well of couse a weather app to tell the user what the current 5-day forcast will be.</p>
-          <p>Technologies: JavaScript, HTML, CSS.</p>
-        </div>
-        
       </div>
     </div>
   )
 }
+
+
+
+
+
+
+
+
+
+
+// 
