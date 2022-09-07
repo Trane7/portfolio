d06fdea0d72f0fd4c09dcf79dbb7f49a27016a38
Dee <dgrowette@gmail.com>
2022-09-07 16:09:29 -0700

    final commit

--- portfolio/src/components/testimonials/Testimonials.jsx
+++ portfolio/src/components/testimonials/Testimonials.jsx
@@ -1,12 +1,22 @@
+import { GitHub, Mail, Resume } from "@material-ui/icons";
 import "./testimonials.scss";
 
 export default function Testimonials() {
   return (
     <div className= "testimonials" id="testimonials">
         <h1>Contact</h1>
-      <div className="card">
-        <a href="https://docs.google.com/document/d/1Rl9pXG5k9HlEfBj2H5fiDfvbMp3aPS6vNDCwy3IiuS8/edit?usp=sharing">Link To Resume</a>
-      </div>
+        <h2><Mail /><span>Email:</span></h2><p>Dgrowette@gmail.com</p>
+        <h2><GitHub className="github"/><span>GitHub:</span></h2><button><a href="https://github.com/Trane7">https://github.com/Trane7</a></button>
+        <h2><span>Resume:</span></h2><button><a href="https://docs.google.com/document/d/1Rl9pXG5k9HlEfBj2H5fiDfvbMp3aPS6vNDCwy3IiuS8/edit?usp=sharing">Link</a></button>
+        <p><span></span></p>
     </div>
+
+        
   )
 }
+
+
+
+
+
+{/* <a href="https://docs.google.com/document/d/1Rl9pXG5k9HlEfBj2H5fiDfvbMp3aPS6vNDCwy3IiuS8/edit?usp=sharing">Link To Resume</a> */}
